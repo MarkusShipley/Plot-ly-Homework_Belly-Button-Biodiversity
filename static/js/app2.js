70ea3a4f857f7157e4de3d3bddc24cfb0181846b
MarkusShipley <mrksshipley@gmail.com>
2022-03-25 00:12:44 -0400

    finall submit

--- static/js/app2.js
+++ static/js/app2.js
@@ -16,7 +16,7 @@ function populateDropdown(){
     });
 
     // initiate Graphs
-    demInfo('940');
+    demoInfo('940');
     buildGraph('940');
     
 }
@@ -29,34 +29,34 @@ function optionChanged(idNumber){
     
     // call the funcions
     // note: as cpvered in Phil's study hall on 3/23/22, you can call a fucntion that is built later in the JS
-     demInfo(idNumber);
+     demoInfo(idNumber);
     buildGraph(idNumber);
 
 }
 
 
 // demographic info fuction
-function demInfo (idNumber) {
+function demoInfo (idNumber) {
     // d3 select of the metadata div
     var demList = d3.select("#sample-metadata");
 
-    // importing the JSON with d3
+    // Using d3 to import the data
     d3.json('samples.json').then((data) => {
         // getting the metadata
         var metadata = data.metadata;
 
-        // filtering metadata along the idNumber
+        // filtering metadata with the idNumber
         var filtered = metadata.filter(d => d.id == idNumber);
 
         // getting the filtered data
         filtered = filtered[0];
 
-        // clearing out any existing data
-        demList.html("");
+        // clearing out any existing data in my demoList
+        demoList.html("");
 
         // iteraing throught the object and appending the 
         Object.entries(filtered).forEach(([key,value]) => {
-            demList.append("h6").text(`${key} : ${value}`);
+            demoList.append("h6").text(`${key} : ${value}`);
         });
 
     });
@@ -74,7 +74,7 @@ function buildGraph (idNumber) {
         // filtering metadata along the idNumber
         var filtered = sampleData.filter(d => d.id == idNumber);
 
-        // getting the labels, sample values and ids vut usimg he filter function
+        // getting the labels, sample values and ids usimg he filter function
         var otuIds = filtered[0]['otu_ids'];
         var sampleValues = filtered[0]['sample_values']
         var otuLabels = filtered[0]['otu_labels']
@@ -127,12 +127,38 @@ function buildGraph (idNumber) {
 
         Plotly.newPlot('bubble', bubbleData, bubbleLayout)
 
-
+        
     });
 
 }
 
 populateDropdown();
+   
+ // Gauge chart
+ var Gauge_chart = [
+    {
+      type: "indicator",
+      mode: "gauge+idnumber+delta",
+      value: data,
+      title: { text: "Belly Button Washing Frequency <br><i>Scrubs per Week</i>", font: { size: 20 } },
+      gauge: {
+        axis: { range: [null, 10], tickwidth: 2, tickcolor: "black" },
+        bar: { color: "#FF0000" },
+        bgcolor: "white",
+        borderwidth: 2,
+        bordercolor: "black",
+        steps: [
+          { range: [0, 1], color: "#FFDCCC" },
+          { range: [1, 2], color: "#FFCAB3"},
+          { range: [2, 3], color: "#FFB999" },
+          { range: [3, 4], color: "#FFA780"},
+          { range: [4, 5], color: "#FF9566" },
+          { range: [5, 6], color: "#FF844D"},
+          { range: [6, 7], color: "#FF7233"},
+          { range: [7, 8], color: "#FF9566" },
+          { range: [9, 10], color: "#FF844D"}
+    
+         
+        ]
 
-//Originially, I attempted to build the the bonus gauge but kust cpoiuld not make it happen.
-//Thus, Bonus kipped for this.
+//Plotly.newPlot('gauge',data);
